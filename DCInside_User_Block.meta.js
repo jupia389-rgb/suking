84907659eed87b405624593dc12232648b5d76f6
jupia389-rgb <jupia389@gmail.com>
2026-08-07 12:19:48 +0900

    Add metadata file for standard userscript updates

---
+++ DCInside_User_Block.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         DCInside User Block
+// @namespace    https://github.com/jupia389-rgb/suking
+// @version      2.1.0
+// @description  디시인사이드 사용자 차단 - 표준 자동 업데이트 방식
+// @match        https://gall.dcinside.com/*
+// @match        https://m.dcinside.com/*
+// @run-at       document-start
+// @grant        none
+// @require      https://raw.githubusercontent.com/jupia389-rgb/suking/main/DCInside_User_Block.core.js?core=1.7.0
+// @downloadURL  https://raw.githubusercontent.com/jupia389-rgb/suking/main/DCInside_User_Block.user.js
+// @updateURL    https://raw.githubusercontent.com/jupia389-rgb/suking/main/DCInside_User_Block.meta.js
+// ==/UserScript==
