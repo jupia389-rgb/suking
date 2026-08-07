a453e39b2eed6b6ff533d0e6e2095360b5dd0776
jupia389-rgb <jupia389@gmail.com>
2026-08-07 13:02:07 +0900

    Update mobile-lite userscript metadata to 3.0.0

--- DCInside_User_Block.meta.js
+++ DCInside_User_Block.meta.js
@@ -1,11 +1,10 @@
 // ==UserScript==
 // @name         DCInside User Block
 // @namespace    https://github.com/jupia389-rgb/suking
-// @version      2.1.3
-// @description  디시인사이드 사용자 차단 - 단일 파일 자동 업데이트 버전
-// @match        https://gall.dcinside.com/*
+// @version      3.0.0
+// @description  디시인사이드 모바일용 최소 사용자 차단 (화면에 보이는 ID·IP·닉네임)
 // @match        https://m.dcinside.com/*
-// @run-at       document-start
+// @run-at       document-end
 // @grant        none
 // @downloadURL  https://raw.githubusercontent.com/jupia389-rgb/suking/main/DCInside_User_Block.user.js
 // @updateURL    https://raw.githubusercontent.com/jupia389-rgb/suking/main/DCInside_User_Block.meta.js
